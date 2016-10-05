c5057d231445607ca4a592296866e008a1957c34
Gabor Horvath <gabor.a.horvath@ericsson.com>
2016-10-05 13:08:23 +0200

    Import related fix and code cleanups.

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -3142,7 +3142,7 @@ Decl *ASTNodeImporter::VisitEnumConstantDecl(EnumConstantDecl *D) {
   DeclarationName Name;
   SourceLocation Loc;
   NamedDecl *ToD;
-  const FunctionDecl *FoundWithoutBody = NULL;
+  const FunctionDecl *FoundWithoutBody = nullptr;
 
   if (ImportDeclParts(D, DC, LexicalDC, Name, ToD, Loc))
     return nullptr;
@@ -3219,21 +3219,19 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
       if (!FoundDecls[I]->isInIdentifierNamespace(IDNS))
         continue;
     
-      if (FunctionDecl *FoundFunction = dyn_cast<FunctionDecl>(FoundDecls[I])) {
+      if (auto *FoundFunction = dyn_cast<FunctionDecl>(FoundDecls[I])) {
         if (FoundFunction->hasExternalFormalLinkage() &&
             D->hasExternalFormalLinkage()) {
           if (Importer.IsStructurallyEquivalent(D->getType(), 
                                                 FoundFunction->getType())) {
             // FIXME: Actually try to merge the body and other attributes.
-            //ednikru-CTU
-        	  const FunctionDecl *FromBodyDecl = NULL;
-              D->hasBody(FromBodyDecl);
-              if (D == FromBodyDecl && !FoundFunction->hasBody()) {
-                // This function is needed to merge completely.
-                FoundWithoutBody = FoundFunction;
-                llvm::errs()<<"Found function without body\n";
-                break;
-              }
+            const FunctionDecl *FromBodyDecl = nullptr;
+            D->hasBody(FromBodyDecl);
+            if (D == FromBodyDecl && !FoundFunction->hasBody()) {
+              // This function is needed to merge completely.
+              FoundWithoutBody = FoundFunction;
+              break;
+            }
             return Importer.Imported(D, FoundFunction);
           }
         
@@ -3272,8 +3270,7 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
   QualType FromTy = D->getType();
   bool usedDifferentExceptionSpec = false;
 
-  if (const FunctionProtoType *
-        FromFPT = D->getType()->getAs<FunctionProtoType>()) {
+  if (const auto *FromFPT = D->getType()->getAs<FunctionProtoType>()) {
     FunctionProtoType::ExtProtoInfo FromEPI = FromFPT->getExtProtoInfo();
     // FunctionProtoType::ExtProtoInfo's ExceptionSpecDecl can point to the
     // FunctionDecl that we are importing the FunctionProtoType for.
@@ -3308,7 +3305,7 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
   TypeSourceInfo *TInfo = Importer.Import(D->getTypeSourceInfo());
   FunctionDecl *ToFunction = nullptr;
   SourceLocation InnerLocStart = Importer.Import(D->getInnerLocStart());
-  if (CXXConstructorDecl *FromConstructor = dyn_cast<CXXConstructorDecl>(D)) {
+  if (auto *FromConstructor = dyn_cast<CXXConstructorDecl>(D)) {
     ToFunction = CXXConstructorDecl::Create(Importer.getToContext(),
                                             cast<CXXRecordDecl>(DC),
                                             InnerLocStart,
@@ -3340,8 +3337,7 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
                                            NameInfo, T, TInfo,
                                            D->isInlineSpecified(),
                                            D->isImplicit());
-  } else if (CXXConversionDecl *FromConversion
-                                           = dyn_cast<CXXConversionDecl>(D)) {
+  } else if (auto *FromConversion = dyn_cast<CXXConversionDecl>(D)) {
     ToFunction = CXXConversionDecl::Create(Importer.getToContext(), 
                                            cast<CXXRecordDecl>(DC),
                                            InnerLocStart,
@@ -3350,7 +3346,7 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
                                            FromConversion->isExplicit(),
                                            D->isConstexpr(),
                                            Importer.Import(D->getLocEnd()));
-  } else if (CXXMethodDecl *Method = dyn_cast<CXXMethodDecl>(D)) {
+  } else if (auto *Method = dyn_cast<CXXMethodDecl>(D)) {
     ToFunction = CXXMethodDecl::Create(Importer.getToContext(), 
                                        cast<CXXRecordDecl>(DC),
                                        InnerLocStart,
@@ -3385,7 +3381,7 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
   ToFunction->setParams(Parameters);
 
   if (FoundWithoutBody) {
-    FunctionDecl *Recent = const_cast<FunctionDecl *>(
+    auto *Recent = const_cast<FunctionDecl *>(
           FoundWithoutBody->getMostRecentDecl());
     ToFunction->setPreviousDecl(Recent);
   }
@@ -3399,7 +3395,6 @@ Decl *ASTNodeImporter::VisitFunctionDecl(FunctionDecl *D) {
   }
 
   // Import the body, if any.
-  llvm::errs()<<"Importing body\n";
   if (Stmt *FromBody = D->getBody()) {
     if (Stmt *ToBody = Importer.Import(FromBody)) {
       ToFunction->setBody(ToBody);
@@ -6949,22 +6944,16 @@ Decl *ASTImporter::Import(Decl *FromD) {
   ASTNodeImporter Importer(*this);
 
   // Check whether we've already imported this declaration.  
-  /*
-   * TODO: CTU check how to re-enable this
-   * llvm::DenseMap<Decl *, Decl *>::iterator Pos = ImportedDecls.find(FromD);
+  llvm::DenseMap<Decl *, Decl *>::iterator Pos = ImportedDecls.find(FromD);
   if (Pos != ImportedDecls.end()) {
     Decl *ToD = Pos->second;
     Importer.ImportDefinitionIfNeeded(FromD, ToD);
     llvm::errs()<<"ASTImporter::Import Declaration already found\n";
     return ToD;
-  }*/
+  }
   
   // Import the type
-  //llvm::errs()<<"ASTImporter::importing from:\n";
-  //FromD->dump();
   Decl *ToD = Importer.Visit(FromD);
-  //llvm::errs()<<"ASTImporter::Import imported:\n";
-  //ToD->dump();
   if (!ToD)
     return nullptr;
 
