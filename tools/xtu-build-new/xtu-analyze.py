2ba1b0619f242582783e2b9e232655e4b834f6d1
Kareem Khazem <khazem@google.com>
2016-10-19 13:13:37 -0700

    Removed required buildgraph option in xtu-analyze
    There is no longer any option to supply a buildgraph file to
    xtu-analyze, since xtu-analyze doesn't use that option anyway, yet the
    option was previously 'required'.

--- tools/xtu-build-new/xtu-analyze.py
+++ tools/xtu-build-new/xtu-analyze.py
@@ -6,15 +6,17 @@ import json
 import multiprocessing
 import os
 import re
+import signal
 import subprocess
 import string
 import sys
 
 threading_factor = int(multiprocessing.cpu_count() * 1.5)
+timeout = 86400
 
 parser = argparse.ArgumentParser(description='Executes 2nd pass of XTU analysis')
 parser.add_argument('-b', required=True, dest='buildlog', metavar='build.json', help='Use a JSON Compilation Database')
-parser.add_argument('-g', required=True, dest='buildgraph', metavar='build-graph.json', help='Use a JSON Build Dependency Graph')
+#parser.add_argument('-g', required=True, dest='buildgraph', metavar='build-graph.json', help='Use a JSON Build Dependency Graph')
 parser.add_argument('-p', metavar='preanalyze-dir', dest='xtuindir', help='Use directory for reading preanalyzation data (default=".xtu")', default='.xtu')
 parser.add_argument('-o', metavar='output-dir', dest='xtuoutdir', help='Use directory for output analyzation results (default=".xtu-out")', default='.xtu-out')
 parser.add_argument('-e', metavar='enabled-checker', nargs='+', dest='enabled_checkers', help='List all enabled checkers')
@@ -55,9 +57,9 @@ buildlog_file = open(mainargs.buildlog, 'r')
 buildlog = json.load(buildlog_file)
 buildlog_file.close()
 
-buildgraph_file = open(mainargs.buildgraph, 'r')
-buildgraph = json.load(buildgraph_file)
-buildgraph_file.close()
+#buildgraph_file = open(mainargs.buildgraph, 'r')
+#buildgraph = json.load(buildgraph_file)
+#buildgraph_file.close()
 
 src_pattern = re.compile('.*\.(cc|c|cxx|cpp)$', re.IGNORECASE)
 cmd_2_order = {}
