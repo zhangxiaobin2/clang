21bcc1f0bb6e60ec38cc7a283ccdeb51075b8a19
Zoltán Gera <gerazo80@gmail.com>
2016-10-21 19:09:42 +0200

    Replacing ccc-analyzer with analyze-cc

--- tools/xtu-build-new/xtu-analyze.py
+++ tools/xtu-build-new/xtu-analyze.py
@@ -12,8 +12,7 @@ import string
 import sys
 
 timeout = 86400
-analyser_output_formats = ['plist-multi-file', 'plist', 'html',
-    'plist-html', 'text']
+analyser_output_formats = ['plist', 'plist-multi-file', 'html', 'plist-html', 'text']
 analyser_output_format = analyser_output_formats[0]
 
 parser = argparse.ArgumentParser(description='Executes 2nd pass of XTU analysis')
@@ -26,7 +25,7 @@ parser.add_argument('-d', metavar='disabled-checker', nargs='+', dest='disabled_
 parser.add_argument('-j', metavar='threads', dest='threads', help='Number of threads used (default=1)', default=1)
 parser.add_argument('-v', dest='verbose', action='store_true', help='Verbose output of every command executed')
 parser.add_argument('--clang-path', metavar='clang-path', dest='clang_path', help='Set path of clang binaries to be used (default taken from CLANG_PATH environment variable)', default=os.environ.get('CLANG_PATH'))
-parser.add_argument('--ccc-analyzer-path', metavar='ccc-analyzer-path', dest='ccc_path', help='Set path of ccc-analyzer to be used (default is current directory)', default='.')
+parser.add_argument('--analyze-cc-path', metavar='analyze-cc-path', dest='analyze_path', help='Set path of analyze-cc to be used (default is taken from CLANG_ANALYZE_CC_PATH environment variable)', default=os.environ.get('CLANG_ANALYZE_CC_PATH'))
 parser.add_argument('--output-format', metavar='format',
     choices=analyser_output_formats, default=analyser_output_format,
     help='Format for analysis reports (one of %s; default is "%s").' %
@@ -49,28 +48,34 @@ else :
 if mainargs.verbose :
     print 'XTU uses clang dir: ' + (clang_path if clang_path != '' else '<taken from PATH>')
 
-ccc_path = os.path.abspath(mainargs.ccc_path)
+if mainargs.analyze_path is None :
+    analyze_path = ''
+else :
+    analyze_path = os.path.abspath(mainargs.analyze_path)
 if mainargs.verbose :
-    print 'XTU uses ccc-analyzer dir: ' + ccc_path
+    print 'XTU uses analyze-cc dir: ' + (analyze_path if analyze_path != '' else '<taken from PATH>')
 
 analyzer_params = []
 if mainargs.enabled_checkers:
     analyzer_params += [ '-analyzer-checker', mainargs.enabled_checkers ]
 if mainargs.disabled_checkers:
     analyzer_params += [ '-analyzer-disable-checker', mainargs.disable_checkers ]
-analyzer_params += [ '-analyzer-config', 'xtu-dir=' + os.path.abspath(mainargs.xtuindir) ]
+analyzer_params += [ '-analyzer-config', 'xtu-dir=' + os.path.abspath(mainargs.xtuindir)]
 if mainargs.without_visitedfns :
     analyzer_params += [ '-analyzer-config', 'reanalyze-xtu-visited=true' ]
 analyzer_params += [ '-analyzer-stats' ]
+analyzer_params += [ '-analyzer-output=' + mainargs.output_format ]
+passthru_analyzer_params = []
+for param in analyzer_params :
+    passthru_analyzer_params += ['-Xanalyzer']
+    passthru_analyzer_params += [param]
 
 analyzer_env = {}
-analyzer_env['CLANG'] = os.path.join(clang_path, 'clang')
-analyzer_env['OUT_DIR'] = os.path.abspath(mainargs.xtuindir)
-analyzer_env['CCC_ANALYZER_HTML'] = os.path.abspath(mainargs.xtuoutdir)
-analyzer_env['CCC_ANALYZER_ANALYSIS'] = ' '.join(analyzer_params)
-analyzer_env['CCC_ANALYZER_OUTPUT_FORMAT'] = mainargs.output_format
-#analyzer_env['CCC_ANALYZER_TIMEOUT'] = str(60 * int(args.timeout))
-analyzer_env['IS_INTERCEPTED'] = 'true'
+analyzer_env['ANALYZE_BUILD_CLANG'] = os.path.join(clang_path, 'clang')
+analyzer_env['ANALYZE_BUILD_REPORT_DIR'] = os.path.abspath(mainargs.xtuoutdir)
+analyzer_env['ANALYZE_BUILD_PARAMETERS'] = ' '.join(passthru_analyzer_params)
+analyzer_env['ANALYZE_BUILD_REPORT_FORMAT'] = mainargs.output_format
+#analyzer_env['ANALYZE_BUILD_VERBOSE'] = 'DEBUG'
 
 buildlog_file = open(mainargs.buildlog, 'r')
 buildlog = json.load(buildlog_file)
@@ -104,15 +109,16 @@ def analyze((directory, command)) :
     old_workdir = os.getcwd()
     compiler, args = get_compiler_and_arguments(command)
     os.environ.update(analyzer_env)
-    os.environ['COMPILER'] = compiler
+    os.environ['ANALYZE_BUILD_CC'] = compiler
+    os.environ['ANALYZE_BUILD_CXX'] = compiler
     os.chdir(directory)
-    ccc_cmd = os.path.join(ccc_path, 'ccc-analyzer') + ' ' + string.join(args, ' ')
+    analyze_cmd = os.path.join(analyze_path, 'analyze-cc') + ' ' + string.join(args, ' ')
     if mainargs.verbose :
-        print ccc_cmd
+        print analyze_cmd
     # Buffer output of subprocess and dump it out at the end, so that
     # the subprocess doesn't continue to write output after the user
     # sends SIGTERM
-    po = subprocess.Popen(ccc_cmd, shell=True, stderr=subprocess.PIPE,
+    po = subprocess.Popen(analyze_cmd, shell=True, stderr=subprocess.PIPE,
             stdout=subprocess.PIPE)
     out, err = po.communicate()
     sys.stderr.write(err)
@@ -127,21 +133,21 @@ except OSError:
     sys.exit(1)
 
 original_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
-ccc_workers = multiprocessing.Pool(processes=mainargs.threads)
+analyze_workers = multiprocessing.Pool(processes=mainargs.threads)
 signal.signal(signal.SIGINT, original_handler)
 steps = [(step['directory'], step['command']) for step in buildlog
         if step['command'] in cmd_2_order]
 try:
-    res = ccc_workers.map_async(analyze, steps)
+    res = analyze_workers.map_async(analyze, steps)
     # Block with timeout so that signals are not ignored, python bug 8296
     res.get(mainargs.timeout)
 except KeyboardInterrupt:
-    ccc_workers.terminate()
-    ccc_workers.join()
+    analyze_workers.terminate()
+    analyze_workers.join()
     exit(1)
 else:
-    ccc_workers.close()
-    ccc_workers.join()
+    analyze_workers.close()
+    analyze_workers.join()
 
 #TODO correct execution order
 
