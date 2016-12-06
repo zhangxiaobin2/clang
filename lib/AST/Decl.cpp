48e67ae82534dacb8bc4f154e3868cc71175be11
Gabor Horvath <xazax.hun@gmail.com>
2016-12-06 16:48:15 +0100

    Fix circular dependencies. Fixes #26.

--- lib/AST/Decl.cpp
+++ lib/AST/Decl.cpp
@@ -2604,10 +2604,6 @@ Stmt *FunctionDecl::getBody(const FunctionDecl *&Definition) const {
   return nullptr;
 }
 
-const FunctionDecl *FunctionDecl::getXTUDefinition(CompilerInstance &CI) const {
-  return getASTContext().getXTUDefinition(this, CI);
-}
-
 void FunctionDecl::setBody(Stmt *B) {
   Body = B;
   if (B)
