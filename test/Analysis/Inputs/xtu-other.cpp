9d978826173e048b36bf562f0732b3f1fc89ef75
Gabor Horvath <xazax.hun@gmail.com>
2016-12-14 13:16:26 +0100

    Additional tests for multiple XTU calls and calling back from another TU.

--- test/Analysis/Inputs/xtu-other.cpp
+++ test/Analysis/Inputs/xtu-other.cpp
@@ -1,3 +1,14 @@
+int callback_to_main(int x);
 int f(int x) {
   return x - 1;
+}
+
+int g(int x) {
+  return callback_to_main(x) + 1;
+}
+
+int h_chain(int);
+
+int h(int x) {
+  return 2*h_chain(x);
 }
