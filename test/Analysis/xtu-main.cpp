22489ceef113f98456d989a9d6a8f82a0e4d2812
Gabor Horvath <xazax.hun@gmail.com>
2016-12-14 13:16:26 +0100

    Additional tests for multiple XTU calls and calling back from another TU.

--- test/Analysis/xtu-main.cpp
+++ test/Analysis/xtu-main.cpp
@@ -1,14 +1,21 @@
 // RUN: mkdir -p %T/xtudir
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -emit-pch -o %T/xtudir/xtu-other.cpp.ast %S/Inputs/xtu-other.cpp
+// RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -emit-pch -o %T/xtudir/xtu-chain.cpp.ast %S/Inputs/xtu-chain.cpp
 // RUN: cp %S/Inputs/externalFnMap.txt %T/xtudir/
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -fsyntax-only -analyze -analyzer-checker=core,debug.ExprInspection -analyzer-config xtu-dir=%T/xtudir -analyzer-config reanalyze-xtu-visited=true -verify %s
 
 void clang_analyzer_eval(int);
 
 int f(int);
+int g(int);
+int h(int);
+
+int callback_to_main(int x) { return x + 1; }
 
 int main() {
   clang_analyzer_eval(f(3) == 2); // expected-warning{{TRUE}}
   clang_analyzer_eval(f(4) == 3); // expected-warning{{TRUE}}
   clang_analyzer_eval(f(5) == 3); // expected-warning{{FALSE}}
+  clang_analyzer_eval(g(4) == 6); // expected-warning{{TRUE}}
+  clang_analyzer_eval(h(2) == 8); // expected-warning{{TRUE}}
 }
