2b2471059c19b54f43e00d2a61de9d9b25132f85
Zoltán Gera <gerazo80@gmail.com>
2016-12-23 14:08:20 +0100

    Add XTU scripts without visited function logic + add missing format to scan-build-py

---
+++ tools/xtu-analysis/xtu-analyze.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+
+import argparse
+import io
+import json
+import multiprocessing
+import os
+import re
+import shutil
+import signal
+import subprocess
+import string
+import sys
+import threading
+import time
+import uuid
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+sys.path.append(os.path.join(os.path.dirname(__file__),
+                             '..', '..', 'utils', 'analyzer'))
+try:
+    import MergeCoverage
+except:
+    raise
+
+threading_factor = int(multiprocessing.cpu_count() * 1.5)
+analyser_output_formats = ['plist-multi-file', 'plist', 'plist-html',
+                           'html', 'text']
+analyser_output_format = analyser_output_formats[0]
+gcov_outdir = 'gcov'
+gcov_tmpdir = gcov_outdir + '_tmp'
+
+parser = argparse.ArgumentParser(
+            description='Executes 2nd pass of XTU analysis')
+parser.add_argument('-b', required=True, dest='buildlog', metavar='build.json',
+                    help='Use a JSON Compilation Database')
+parser.add_argument('-p', metavar='preanalyze-dir', dest='xtuindir',
+                    help='Use directory for reading preanalyzation data '
+                         '(default=".xtu")',
+                    default='.xtu')
+parser.add_argument('-o', metavar='output-dir', dest='xtuoutdir',
+                    help='Use directory for output analyzation results '
+                         '(default=".xtu-out")',
+                    default='.xtu-out')
+parser.add_argument('-e', metavar='enabled-checker', nargs='+',
+                    dest='enabled_checkers',
+                    help='List all enabled checkers')
+parser.add_argument('-d', metavar='disabled-checker', nargs='+',
+                    dest='disabled_checkers',
+                    help='List all disabled checkers')
+parser.add_argument('-j', metavar='threads', dest='threads',
+                    help='Number of threads used (default=' +
+                    str(threading_factor) + ')',
+                    default=threading_factor)
+parser.add_argument('-v', dest='verbose', action='store_true',
+                    help='Verbose output of every command executed')
+parser.add_argument('--clang-path', metavar='clang-path', dest='clang_path',
+                    help='Set path of clang binaries to be used (default '
+                         'taken from CLANG_PATH environment variable)',
+                    default=os.environ.get('CLANG_PATH'))
+parser.add_argument('--analyze-cc-path', metavar='analyze-cc-path',
+                    dest='analyze_path',
+                    help='Set path of analyze-cc to be used '
+                         '(default is taken from CLANG_ANALYZE_CC_PATH '
+                         'environment variable)',
+                    default=os.environ.get('CLANG_ANALYZE_CC_PATH'))
+parser.add_argument('--output-format', metavar='format',
+                    choices=analyser_output_formats,
+                    default=analyser_output_format,
+                    help='Format for analysis reports '
+                         '(one of %s; default is "%s").' %
+                    (', '.join(analyser_output_formats),
+                     analyser_output_format))
+parser.add_argument('--no-xtu', dest='no_xtu', action='store_true',
+                    help='Do not use XTU at all, '
+                         'only do normal static analysis')
+parser.add_argument('--record-coverage', dest='record_coverage',
+                    action='store_true',
+                    help='Generate coverage information during analysis')
+mainargs = parser.parse_args()
+
+concurrent_threads = 0
+concurrent_thread_times = [0.0]
+concurrent_thread_last_clock = time.time()
+
+if mainargs.clang_path is None:
+    clang_path = ''
+else:
+    clang_path = os.path.abspath(mainargs.clang_path)
+if mainargs.verbose:
+    print 'XTU uses clang dir: ' + (clang_path if clang_path != ''
+                                    else '<taken from PATH>')
+
+if mainargs.analyze_path is None:
+    analyze_path = ''
+else:
+    analyze_path = os.path.abspath(mainargs.analyze_path)
+if mainargs.verbose:
+    print 'XTU uses analyze-cc dir: ' + (analyze_path if analyze_path != ''
+                                         else '<taken from PATH>')
+
+analyzer_params = []
+if mainargs.enabled_checkers:
+    analyzer_params += ['-analyzer-checker', mainargs.enabled_checkers]
+if mainargs.disabled_checkers:
+    analyzer_params += ['-analyzer-disable-checker', mainargs.disable_checkers]
+if not mainargs.no_xtu:
+    analyzer_params += ['-analyzer-config',
+                        'xtu-dir=' + os.path.abspath(mainargs.xtuindir)]
+analyzer_params += ['-analyzer-config', 'reanalyze-xtu-visited=true']
+if mainargs.record_coverage:
+    gcov_tmppath = os.path.abspath(os.path.join(mainargs.xtuoutdir,
+                                                gcov_tmpdir))
+    gcov_finalpath = os.path.abspath(os.path.join(mainargs.xtuoutdir,
+                                                  gcov_outdir))
+    shutil.rmtree(gcov_tmppath, True)
+    # analyzer_params += ['-analyzer-config',
+    #                     'record-coverage=' + gcov_tmppath]
+analyzer_params += ['-analyzer-stats']
+# analyzer_params += ['-analyzer-output ' + mainargs.output_format]
+passthru_analyzer_params = []
+for param in analyzer_params:
+    passthru_analyzer_params += ['-Xanalyzer']
+    passthru_analyzer_params += [param]
+passthru_analyzer_params += ['--analyzer-output ' + mainargs.output_format]
+
+analyzer_env = os.environ.copy()
+analyzer_env['ANALYZE_BUILD_CLANG'] = os.path.join(clang_path, 'clang')
+analyzer_env['ANALYZE_BUILD_REPORT_DIR'] = os.path.abspath(mainargs.xtuoutdir)
+analyzer_env['ANALYZE_BUILD_PARAMETERS'] = ' '.join(passthru_analyzer_params)
+analyzer_env['ANALYZE_BUILD_REPORT_FORMAT'] = mainargs.output_format
+# analyzer_env['ANALYZE_BUILD_VERBOSE'] = 'DEBUG'
+
+graph_lock = threading.Lock()
+
+buildlog_file = open(mainargs.buildlog, 'r')
+buildlog = json.load(buildlog_file)
+buildlog_file.close()
+
+src_pattern = re.compile('.*\.(C|c|cc|cpp|cxx|ii|m|mm)$', re.IGNORECASE)
+dircmd_separator = ': '
+dircmd_2_orders = {}
+src_build_steps = 0
+for step in buildlog:
+    if src_pattern.match(step['file']):
+        uid = step['directory'] + dircmd_separator + step['command']
+        if uid not in dircmd_2_orders:
+            dircmd_2_orders[uid] = [src_build_steps]
+        else:
+            dircmd_2_orders[uid].append(src_build_steps)
+        src_build_steps += 1
+
+
+def get_compiler_and_arguments(cmd):
+    had_command = False
+    args = []
+    for arg in cmd.split():
+        if had_command:
+            args.append(arg)
+        if not had_command and arg.find('=') == -1:
+            had_command = True
+            compiler = arg
+    return compiler, args
+
+
+def analyze(directory, command):
+    compiler, args = get_compiler_and_arguments(command)
+
+    last_src = None
+    for cmdpart in command.split():
+        if src_pattern.match(cmdpart):
+            last_src = cmdpart
+    tu_name = ''
+    if last_src:
+        tu_name += last_src.split(os.sep)[-1]
+    tu_name += '_' + str(uuid.uuid4())
+
+    cmdenv = analyzer_env.copy()
+    cmdenv['ANALYZE_BUILD_CC'] = compiler
+    cmdenv['ANALYZE_BUILD_CXX'] = compiler
+    if mainargs.record_coverage:
+        cmdenv['ANALYZE_BUILD_PARAMETERS'] += \
+            ' -Xanalyzer -analyzer-config -Xanalyzer record-coverage=' + \
+            os.path.join(gcov_tmppath, tu_name)
+    analyze_cmd = os.path.join(analyze_path, 'analyze-cc') + \
+        ' ' + string.join(args, ' ')
+    if mainargs.verbose:
+        print analyze_cmd
+
+    # Buffer output of subprocess and dump it out at the end, so that
+    # the subprocess doesn't continue to write output after the user
+    # sends SIGTERM
+    runOK = True
+    out = '******* Error running command'
+    try:
+        po = subprocess.Popen(analyze_cmd, shell=True,
+                              stderr=subprocess.STDOUT,
+                              stdout=subprocess.PIPE,
+                              universal_newlines=True,
+                              cwd=directory,
+                              env=cmdenv)
+        out, _ = po.communicate()
+        runOK = not po.returncode
+    except OSError:
+        runOK = False
+    if mainargs.verbose:
+        sys.stdout.write(out)
+    if not runOK:
+        prefix = os.path.join(os.path.abspath(mainargs.xtuoutdir), "fails")
+    else:
+        prefix = os.path.join(os.path.abspath(mainargs.xtuoutdir), "passes")
+    with open(os.path.join(prefix, "%s.out" % tu_name), "w") as f:
+        f.write("%s\n%s" % (analyze_cmd, out))
+
+
+def analyze_work():
+    global concurrent_threads
+    global concurrent_thread_times
+    global concurrent_thread_last_clock
+    global graph_lock
+    global dircmd_2_orders
+    while len(dircmd_2_orders) > 0:
+        graph_lock.acquire()
+        found_dircmd_orders = None
+        found_dircmd = None
+        found_orders = None
+        for dircmd_orders in dircmd_2_orders.items():
+            dircmd = dircmd_orders[0].split(dircmd_separator, 2)
+            orders = dircmd_orders[1]
+            assert len(dircmd) == 2 and len(dircmd[0]) > 0 and \
+                len(dircmd[1]) > 0
+            assert len(orders) > 0
+            found_dircmd_orders = dircmd_orders
+            found_dircmd = dircmd
+            found_orders = orders
+            break
+        if found_dircmd_orders is not None:
+            del dircmd_2_orders[found_dircmd_orders[0]]
+
+            concurrent_thread_current_clock = time.time()
+            concurrent_thread_times[concurrent_threads] += \
+                concurrent_thread_current_clock - concurrent_thread_last_clock
+            concurrent_thread_last_clock = concurrent_thread_current_clock
+            concurrent_threads += 1
+            if len(concurrent_thread_times) == concurrent_threads:
+                concurrent_thread_times.append(0.0)
+
+            graph_lock.release()
+            analyze(found_dircmd[0], found_dircmd[1])
+            graph_lock.acquire()
+
+            concurrent_thread_current_clock = time.time()
+            concurrent_thread_times[concurrent_threads] += \
+                concurrent_thread_current_clock - concurrent_thread_last_clock
+            concurrent_thread_last_clock = concurrent_thread_current_clock
+            concurrent_threads -= 1
+            assert concurrent_threads >= 0
+
+            graph_lock.release()
+        else:
+            graph_lock.release()
+            time.sleep(0.125)
+
+try:
+    os.makedirs(os.path.abspath(mainargs.xtuoutdir))
+except OSError:
+    print 'Output directory %s already exists!' % \
+        os.path.abspath(mainargs.xtuoutdir)
+    sys.exit(1)
+
+os.makedirs(os.path.join(os.path.abspath(mainargs.xtuoutdir), "passes"))
+os.makedirs(os.path.join(os.path.abspath(mainargs.xtuoutdir), "fails"))
+
+original_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+signal.signal(signal.SIGINT, original_handler)
+
+analyze_workers = []
+for i in range(int(mainargs.threads)):
+    analyze_workers.append(threading.Thread(target=analyze_work))
+for worker in analyze_workers:
+    worker.start()
+try:
+    for worker in analyze_workers:
+        worker.join(9999999999)
+except KeyboardInterrupt:
+    exit(1)
+
+try:
+    os.removedirs(os.path.abspath(mainargs.xtuoutdir))
+    print 'Removing directory %s because it contains no reports' % \
+        os.path.abspath(mainargs.xtuoutdir)
+except OSError:
+    pass
+
+if mainargs.record_coverage:
+    MergeCoverage.main(gcov_tmppath, gcov_finalpath)
+    shutil.rmtree(gcov_tmppath, True)
+
+assert concurrent_threads == 0
+concurrent_thread_times[0] += time.time() - concurrent_thread_last_clock
+sumtime = 0.0
+for i in range(len(concurrent_thread_times)):
+    sumtime += concurrent_thread_times[i]
+print '--- Total running time: %.2fs' % sumtime
+for i in range(len(concurrent_thread_times)):
+    print '----- ' + \
+        (('using %d processes' % i) if i != 0 else 'self time') + \
+        ' for %.2fs (%.0f%%)' % (concurrent_thread_times[i],
+                                 concurrent_thread_times[i] * 100.0 / sumtime)
---
+++ tools/xtu-analysis/xtu-build.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python
+
+import argparse
+import io
+import json
+import multiprocessing
+import os
+import re
+import signal
+import subprocess
+import string
+
+threading_factor = int(multiprocessing.cpu_count() * 1.5)
+timeout = 86400
+
+parser = argparse.ArgumentParser(
+    description='Executes 1st pass of XTU analysis')
+parser.add_argument('-b', required=True, dest='buildlog',
+                    metavar='build.json',
+                    help='Use a JSON Compilation Database')
+parser.add_argument('-p', metavar='preanalyze-dir', dest='xtuindir',
+                    help='Use directory for generating preanalyzation data '
+                         '(default=".xtu")',
+                    default='.xtu')
+parser.add_argument('-j', metavar='threads', dest='threads',
+                    help='Number of threads used (default=' +
+                    str(threading_factor) + ')',
+                    default=threading_factor)
+parser.add_argument('-v', dest='verbose', action='store_true',
+                    help='Verbose output of every command executed')
+parser.add_argument('--clang-path', metavar='clang-path', dest='clang_path',
+                    help='Set path of clang binaries to be used '
+                         '(default taken from CLANG_PATH envvar)',
+                    default=os.environ.get('CLANG_PATH'))
+parser.add_argument('--timeout', metavar='N',
+                    help='Timeout for build in seconds (default: %d)' %
+                    timeout,
+                    default=timeout)
+mainargs = parser.parse_args()
+
+if mainargs.clang_path is None:
+    clang_path = ''
+else:
+    clang_path = os.path.abspath(mainargs.clang_path)
+if mainargs.verbose:
+    print 'XTU uses clang dir: ' + \
+        (clang_path if clang_path != '' else '<taken from PATH>')
+
+buildlog_file = open(mainargs.buildlog, 'r')
+buildlog = json.load(buildlog_file)
+buildlog_file.close()
+
+src_pattern = re.compile('.*\.(C|c|cc|cpp|cxx|ii|m|mm)$', re.IGNORECASE)
+src_2_dir = {}
+src_2_cmd = {}
+src_order = []
+cmd_2_src = {}
+cmd_order = []
+for step in buildlog:
+    if src_pattern.match(step['file']):
+        if step['file'] not in src_2_dir:
+            src_2_dir[step['file']] = step['directory']
+        if step['file'] not in src_2_cmd:
+            src_2_cmd[step['file']] = step['command']
+            src_order.append(step['file'])
+        if step['command'] not in cmd_2_src:
+            cmd_2_src[step['command']] = [step['file']]
+            cmd_order.append(step['command'])
+        else:
+            cmd_2_src[step['command']].append(step['file'])
+
+
+def clear_file(filename):
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
+
+
+def get_command_arguments(cmd):
+    had_command = False
+    args = []
+    for arg in cmd.split():
+        if had_command and not src_pattern.match(arg):
+            args.append(arg)
+        if not had_command and arg.find('=') == -1:
+            had_command = True
+    return args
+
+
+def generate_ast(source):
+    cmd = src_2_cmd[source]
+    args = get_command_arguments(cmd)
+    arch_command = os.path.join(clang_path, 'clang-cmdline-arch-extractor') + \
+        ' ' + string.join(args, ' ') + ' ' + source
+    if mainargs.verbose:
+        print arch_command
+    arch_output = subprocess.check_output(arch_command, shell=True)
+    arch = arch_output[arch_output.rfind('@')+1:].strip()
+    ast_joined_path = os.path.join(mainargs.xtuindir,
+                                   os.path.join('/ast/' + arch,
+                                                os.path.realpath(source)[1:] +
+                                                '.ast')[1:])
+    ast_path = os.path.abspath(ast_joined_path)
+    try:
+        os.makedirs(os.path.dirname(ast_path))
+    except OSError:
+        if os.path.isdir(os.path.dirname(ast_path)):
+            pass
+        else:
+            raise
+    dir_command = 'cd ' + src_2_dir[source]
+    ast_command = os.path.join(clang_path, 'clang') + ' -emit-ast ' + \
+        string.join(args, ' ') + ' -w ' + source + ' -o ' + ast_path
+    if mainargs.verbose:
+        print dir_command + " && " + ast_command
+    subprocess.call(dir_command + " && " + ast_command, shell=True)
+
+
+def map_functions(command):
+    args = get_command_arguments(command)
+    sources = cmd_2_src[command]
+    dir_command = 'cd ' + src_2_dir[sources[0]]
+    funcmap_command = os.path.join(clang_path, 'clang-func-mapping') + \
+        ' --xtu-dir ' + os.path.abspath(mainargs.xtuindir) + ' ' + \
+        string.join(sources, ' ') + ' -- ' + string.join(args, ' ')
+    if mainargs.verbose:
+        print funcmap_command
+    subprocess.call(dir_command + " && " + funcmap_command, shell=True)
+
+clear_file(os.path.join(mainargs.xtuindir, 'cfg.txt'))
+clear_file(os.path.join(mainargs.xtuindir, 'definedFns.txt'))
+clear_file(os.path.join(mainargs.xtuindir, 'externalFns.txt'))
+clear_file(os.path.join(mainargs.xtuindir, 'externalFnMap.txt'))
+
+original_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+ast_workers = multiprocessing.Pool(processes=int(mainargs.threads))
+signal.signal(signal.SIGINT, original_handler)
+try:
+    res = ast_workers.map_async(generate_ast, src_order)
+    # Block with timeout so that signals don't get ignored, python bug 8296
+    res.get(mainargs.timeout)
+except KeyboardInterrupt:
+    ast_workers.terminate()
+    ast_workers.join()
+    exit(1)
+else:
+    ast_workers.close()
+    ast_workers.join()
+
+original_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+funcmap_workers = multiprocessing.Pool(processes=int(mainargs.threads))
+signal.signal(signal.SIGINT, original_handler)
+try:
+    res = funcmap_workers.map_async(map_functions, cmd_order)
+    res.get(mainargs.timeout)
+except KeyboardInterrupt:
+    funcmap_workers.terminate()
+    funcmap_workers.join()
+    exit(1)
+else:
+    funcmap_workers.close()
+    funcmap_workers.join()
+
+
+# Generate externalFnMap.txt
+
+func_2_file = {}
+extfunc_2_file = {}
+func_2_fileset = {}
+
+defined_fns_filename = os.path.join(mainargs.xtuindir, 'definedFns.txt')
+with open(defined_fns_filename,  'r') as defined_fns_file:
+    for line in defined_fns_file:
+        funcname, filename = line.strip().split(' ')
+        if funcname.startswith('!'):
+            funcname = funcname[1:]  # main function
+        if funcname not in func_2_file.keys():
+            func_2_fileset[funcname] = set([filename])
+        else:
+            func_2_fileset[funcname].add(filename)
+        func_2_file[funcname] = filename
+
+extern_fns_filename = os.path.join(mainargs.xtuindir, 'externalFns.txt')
+with open(extern_fns_filename,  'r') as extern_fns_file:
+    for line in extern_fns_file:
+        line = line.strip()
+        if line in func_2_file and line not in extfunc_2_file:
+            extfunc_2_file[line] = func_2_file[line]
+
+extern_fns_map_filename = os.path.join(mainargs.xtuindir, 'externalFnMap.txt')
+with open(extern_fns_map_filename, 'w') as out_file:
+    for func, fname in extfunc_2_file.items():
+        if len(func_2_fileset[func]) == 1:
+            out_file.write('%s %s.ast\n' % (func, fname))
