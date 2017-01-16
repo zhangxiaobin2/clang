2940efda882b3582f3fb78ea52e692a20139ce30
Zoltán Gera <gerazo80@gmail.com>
2017-01-16 13:48:39 +0100

    Add XTU tests for class/namesapce embedded functions

--- test/Analysis/Inputs/xtu-other.cpp
+++ test/Analysis/Inputs/xtu-other.cpp
@@ -10,5 +10,41 @@ int g(int x) {
 int h_chain(int);
 
 int h(int x) {
-  return 2*h_chain(x);
+  return 2 * h_chain(x);
 }
+
+namespace myns {
+int fns(int x) {
+  return x + 7;
+}
+
+namespace embed_ns {
+int fens(int x) {
+  return x - 3;
+}
+}
+
+class embed_cls {
+public:
+  int fecl(int x) {
+    return x - 7;
+  }
+};
+}
+
+class mycls {
+public:
+  int fcl(int x) {
+    return x + 5;
+  }
+  static int fscl(int x) {
+    return x + 6;
+  }
+
+  class embed_cls2 {
+  public:
+    int fecl2(int x) {
+      return x - 11;
+    }
+  };
+};
