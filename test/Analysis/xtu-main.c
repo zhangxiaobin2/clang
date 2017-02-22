ab583076d76a07deb903d09754c70acf6a0b44cc
Daniel Krupp <daniel.krupp@ericsson.com>
2017-02-22 15:45:59 +0100

    Fixes #68

--- test/Analysis/xtu-main.c
+++ test/Analysis/xtu-main.c
@@ -2,7 +2,6 @@
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -emit-pch -o %T/xtudir2/xtu-other.c.ast %S/Inputs/xtu-other.c
 // RUN: cp %S/Inputs/externalFnMap2.txt %T/xtudir2/externalFnMap.txt
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -fsyntax-only -analyze -analyzer-checker=core,debug.ExprInspection -analyzer-config xtu-dir=%T/xtudir2 -analyzer-config reanalyze-xtu-visited=true -verify %s
-
 void clang_analyzer_eval(int);
 
 typedef struct {
@@ -15,5 +14,15 @@ extern foobar fb;
 
 int main() {
   clang_analyzer_eval(f(5) == 1); // expected-warning{{TRUE}}
+  
   return 0;
+}
+
+//TEST
+//reporting error
+//in a macro
+struct S;
+int g(struct S*);
+void test_macro(void){
+    g(0);//expected-warning@Inputs/xtu-other.c:26 {{Access to field 'a' results in a dereference of a null pointer (loaded from variable 'ctx')}}
 }
