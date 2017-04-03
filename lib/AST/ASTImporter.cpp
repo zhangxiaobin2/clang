5a55eaf28abcf0d5d9a41a69be2739f170cc62fe
Gabor Horvath <gabor.a.horvath@ericsson.com>
2017-04-03 10:07:41 +0200

    Fixes according to the open source review comments.

--- lib/AST/ASTImporter.cpp
+++ lib/AST/ASTImporter.cpp
@@ -1806,7 +1806,6 @@ Decl *ASTNodeImporter::VisitEnumConstantDecl(EnumConstantDecl *D) {
   DeclarationName Name;
   SourceLocation Loc;
   NamedDecl *ToD;
-
   if (ImportDeclParts(D, DC, LexicalDC, Name, ToD, Loc))
     return nullptr;
   if (ToD)
