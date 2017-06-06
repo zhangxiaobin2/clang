895cd79be78b243952b1e88bf630833d6bf72313
Daniel Krupp <daniel.krupp@ericsson.com>
2017-06-06 17:36:03 +0200

    added tests for inlining implicit c functions and functions with struct definitions in their prototype

--- test/Analysis/xtu-main.c
+++ test/Analysis/xtu-main.c
@@ -1,7 +1,7 @@
 // RUN: mkdir -p %T/xtudir2
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -emit-pch -o %T/xtudir2/xtu-other.c.ast %S/Inputs/xtu-other.c
 // RUN: cp %S/Inputs/externalFnMap2.txt %T/xtudir2/externalFnMap.txt
-// RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -fsyntax-only -analyze -analyzer-checker=core,debug.ExprInspection -analyzer-config xtu-dir=%T/xtudir2 -analyzer-config reanalyze-xtu-visited=true -verify %s
+// RUN: %clang_cc1 -std=c89 -triple x86_64-pc-linux-gnu -fsyntax-only -analyze -analyzer-checker=core,debug.ExprInspection -analyzer-config xtu-dir=%T/xtudir2 -analyzer-config reanalyze-xtu-visited=true -verify %s
 
 void clang_analyzer_eval(int);
 
@@ -39,4 +39,24 @@ struct S;
 int g(struct S *);
 void test_macro(void) {
   g(0); // expected-warning@Inputs/xtu-other.c:31 {{Access to field 'a' results in a dereference of a null pointer (loaded from variable 'ctx')}}
+}
+
+//Test:
+//the external function prototype is incomplete
+//warning:implicit functions are prohibited by c99
+void test_implicit(){
+    int res=ident_implicit(6);//external implicit functions are not inlined
+    clang_analyzer_eval(res == 6); // expected-warning{{UNKNOWN}}
+}
+
+//Tests the import of
+//functions that have a struct parameter
+//defined in its prototype
+struct data_t{int a;int b;};
+int struct_in_proto(struct data_t *d);
+void test_struct_def_in_argument(){
+  struct data_t d;
+  d.a=1;
+  d.b=0;
+  clang_analyzer_eval(struct_in_proto(&d)==0);// expected-warning{{UNKNOWN}}
 }
